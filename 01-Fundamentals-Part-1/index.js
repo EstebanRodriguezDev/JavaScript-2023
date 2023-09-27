89665aec9d49b503f08a96c0a626dc3c77bf191e
Esteban Rodriguez <estebandavidrodriguez99@gmail.com>
2023-09-27 17:24:33 -0300

    OPERADORES LOGICOS - AND, OR, NOT

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -99,18 +99,66 @@ Tipos de Datos: Primitivos
 
 // IF / ELSE
 
-const edad = 12;
-const mayorDeEdad = edad >= 18;
+// const edad = 12;
+// const mayorDeEdad = edad >= 18;
 
-if (mayorDeEdad) {
-  console.log("Esteban tiene permitido tramitas la licencia de conducir 🚗");
-} else {
-  const edadRestante = 18 - edad;
-  console.log(
-    `Esteban todavia es menor, debe esperar ${edadRestante} años mas 🥰`
-  );
-}
+// if (mayorDeEdad) {
+//   console.log("Esteban tiene permitido tramitas la licencia de conducir 🚗");
+// } else {
+//   const edadRestante = 18 - edad;
+//   console.log(
+//     `Esteban todavia es menor, debe esperar ${edadRestante} años mas 🥰`
+//   );
+// }
 
 // CONVERSION DE TIPO Y COERCION DE TIPO:
 
 // CONVERSION DE TIPO: convertimos explicitamente un tipo de dato a otro.
+
+// const inputYear = "1999";
+// console.log(Number(inputYear), inputYear);
+// console.log(inputYear + 24);
+// console.log(Number(inputYear) + 24);
+
+// COERCION DE TIPO: JavaScript convierte automaticamente dos tipos de datos que sean diferente con el objetivo de que ejecute el programa
+
+// console.log("I am " + 23 + " years Old");
+// console.log("23" - "10" - 3);
+// console.log("23" / 2);
+
+let n = "1" + 1; // "11"
+n = n - 1; // 11 - 1 = 10
+
+// console.log(n);
+
+// VALORES DE TRUE O FALSE
+
+// 5 valores falsos: 0, '', undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Esteban"));
+// console.log(Boolean({}));
+
+const dinero = 0;
+
+// if (dinero) {
+//   console.log("Tienes dinero en tu cuenta, no lo gastes todo");
+// } else {
+//   console.log("No tienes dinero, consigue un trabajo ya 😂");
+// }
+
+let altura;
+
+// if (altura) {
+//   console.log("La variable altura esta definida");
+// } else {
+//   console.log("La variable altura es UNDEFINED");
+// }
+
+// OPERADORES DE IGUALDAD : == VS ===
+
+const edad = 18;
+
+if (edad === 18) console.log("Ya sos mayor de edad (operador estricto)");
+
+if (edad == 18) console.log("Ya sos mayor de edad (operador flexible)");
